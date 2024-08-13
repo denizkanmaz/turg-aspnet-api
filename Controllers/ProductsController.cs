07e3bc7bd3fef0ed49029ba8ec1b4205a0a5c8a7
denizkanmaz <denizkanmaz@gmail.com>
2024-08-13 22:58:17 +0300

    refactor: move db operation from controllers to models

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -11,31 +11,7 @@ namespace Turg.App.Controllers
         [HttpGet]
         public async Task<List<Product>> Index()
         {
-            await using var conn = new NpgsqlConnection(Constants.ConnectionString);
-            await conn.OpenAsync();
-
-            var products = new List<Product>();
-
-            await using (var cmd = new NpgsqlCommand("SELECT * FROM products", conn))
-            {
-                await using NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
-
-                while (await reader.ReadAsync())
-                {
-                    products.Add(new Product
-                    {
-                        Id = reader.GetGuid(0),
-                        Name = reader.GetString(1),
-                        Category = reader.GetString(2),
-                        Description = reader.GetString(3),
-                        Price = reader.GetDouble(4),
-                        Currency = reader.GetString(5)
-                    });
-                }
-            }
-
-            await conn.CloseAsync();
-            return products;
+            return await Product.GetAll();
         }
 
         // Returns products by category name.
@@ -43,31 +19,7 @@ namespace Turg.App.Controllers
         [HttpGet("GetProductsByCategory")]
         public async Task<List<Product>> GetProductsByCategory([FromQuery] string category)
         {
-            await using var conn = new NpgsqlConnection(Constants.ConnectionString);
-            await conn.OpenAsync();
-
-            var products = new List<Product>();
-
-            await using (var cmd = new NpgsqlCommand($"SELECT * FROM products WHERE category = '{category}'", conn))
-            {
-                await using NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
-
-                while (await reader.ReadAsync())
-                {
-                    products.Add(new Product
-                    {
-                        Id = reader.GetGuid(0),
-                        Name = reader.GetString(1),
-                        Category = reader.GetString(2),
-                        Description = reader.GetString(3),
-                        Price = reader.GetDouble(4),
-                        Currency = reader.GetString(5)
-                    });
-                }
-            }
-
-            await conn.CloseAsync();
-            return products;
+            return await Product.GetByCategoryId(category);
         }
 
         // Adds a new product.
@@ -82,24 +34,7 @@ namespace Turg.App.Controllers
         [HttpGet("AddProduct")]
         public async Task<dynamic> AddProduct([FromBody] Product product)
         {
-            await using var conn = new NpgsqlConnection(Constants.ConnectionString);
-            await conn.OpenAsync();
-
-            var id = Guid.NewGuid();
-
-            await using (var cmd = new NpgsqlCommand("INSERT INTO products (id, name, category, description, price, currency) VALUES (@id, @name, @category, @description, @price, @currency)", conn))
-            {
-                cmd.Parameters.AddWithValue("id", id);
-                cmd.Parameters.AddWithValue("name", product.Name);
-                cmd.Parameters.AddWithValue("category", product.Category);
-                cmd.Parameters.AddWithValue("description", product.Description);
-                cmd.Parameters.AddWithValue("price", product.Price);
-                cmd.Parameters.AddWithValue("currency", product.Currency);
-
-                await cmd.ExecuteNonQueryAsync();
-            }
-
-            await conn.CloseAsync();
+            var id = await Product.Create(product);
             return new { Result = "OK", Message = "Product added", Id = id };
         }
 
@@ -116,22 +51,7 @@ namespace Turg.App.Controllers
         [HttpGet("UpdateProduct")]
         public async Task<dynamic> UpdateProduct([FromBody] Product product)
         {
-            await using var conn = new NpgsqlConnection(Constants.ConnectionString);
-            await conn.OpenAsync();
-
-            await using (var cmd = new NpgsqlCommand("UPDATE products SET name = @name, category = @category, description = @description, price = @price, currency = @currency WHERE id = @id", conn))
-            {
-                cmd.Parameters.AddWithValue("id", product.Id);
-                cmd.Parameters.AddWithValue("name", product.Name);
-                cmd.Parameters.AddWithValue("category", product.Category);
-                cmd.Parameters.AddWithValue("description", product.Description);
-                cmd.Parameters.AddWithValue("price", product.Price);
-                cmd.Parameters.AddWithValue("currency", product.Currency);
-
-                await cmd.ExecuteNonQueryAsync();
-            }
-
-            await conn.CloseAsync();
+            await Product.Update(product);
             return new { Result = "OK", Message = "Product updated" };
         }
     }
--- Controllers/ShoppingCartsController.cs
+++ Controllers/ShoppingCartsController.cs
@@ -11,46 +11,7 @@ namespace Turg.App.Controllers
         [HttpGet("GetById")]
         public async Task<ShoppingCart> GetById([FromQuery] string id)
         {
-            await using var conn = new NpgsqlConnection(Constants.ConnectionString);
-            await conn.OpenAsync();
-
-            var shoppingCart = new ShoppingCart();
-
-            await using (var cmd = new NpgsqlCommand($"SELECT * FROM shopping_carts where id = '{id}'", conn))
-            {
-                await using NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
-
-                while (await reader.ReadAsync())
-                {
-                    shoppingCart.Id = reader.GetGuid(0).ToString();
-                    shoppingCart.Products = new List<Product>();
-
-                    var conn2 = new NpgsqlConnection(Constants.ConnectionString);
-                    conn2.Open();
-
-                    var cmd2 = new NpgsqlCommand($"SELECT p.* FROM shopping_cart_items as sci INNER JOIN products as p on p.id = sci.product_id WHERE sci.shopping_cart_id = '{shoppingCart.Id}'", conn2);
-                    await using NpgsqlDataReader reader2 = await cmd2.ExecuteReaderAsync();
-
-                    while (await reader2.ReadAsync())
-                    {
-                        shoppingCart.Products.Add(new Product
-                        {
-                            Id = reader2.GetGuid(0),
-                            Name = reader2.GetString(1),
-                            Category = reader2.GetString(2),
-                            Description = reader2.GetString(3),
-                            Price = reader2.GetDouble(4),
-                            Currency = reader2.GetString(5)
-                        });
-                    }
-
-                    cmd2.Dispose();
-                }
-            }
-
-            await conn.CloseAsync();
-
-            return shoppingCart;
+            return await ShoppingCart.GetById(id);
         }
 
         // Adds a shopping cart item to a shopping cart.
@@ -63,26 +24,7 @@ namespace Turg.App.Controllers
         [HttpGet("AddProduct")]
         public async Task<dynamic> AddProduct([FromBody] ShoppingCartItem shoppingCartItem)
         {
-            var isNewShoppingCart = shoppingCartItem.ShoppingCartId == null;
-
-            var shoppingCartId = shoppingCartItem.ShoppingCartId ?? Guid.NewGuid();
-
-            if (isNewShoppingCart)
-            {
-                await using var conn = new NpgsqlConnection(Constants.ConnectionString);
-                await conn.OpenAsync();
-                var cmd = new NpgsqlCommand($"INSERT INTO shopping_carts (id) VALUES ('{shoppingCartId}')", conn);
-                cmd.ExecuteNonQuery();
-                cmd.Dispose();
-                conn.Close();
-            }
-
-            await using var conn2 = new NpgsqlConnection(Constants.ConnectionString);
-            await conn2.OpenAsync();
-            var cmd2 = new NpgsqlCommand($"INSERT INTO shopping_cart_items (shopping_cart_id, product_id, quantity) VALUES ('{shoppingCartId}', '{shoppingCartItem.ProductId}', {1})", conn2);
-            cmd2.ExecuteNonQuery();
-            cmd2.Dispose();
-            conn2.Close();
+            await ShoppingCart.AddProduct(shoppingCartItem);
 
             return new { Result = "OK", Message = "Product added to shopping cart" };
         }
@@ -92,12 +34,7 @@ namespace Turg.App.Controllers
         [HttpGet("Delete")]
         public async void Delete([FromQuery] string id)
         {
-            await using var conn = new NpgsqlConnection(Constants.ConnectionString);
-            await conn.OpenAsync();
-            var cmd = new NpgsqlCommand($"DELETE FROM shopping_cart_items where shopping_cart_id = '{id}'; DELETE FROM shopping_carts WHERE id = '{id}'", conn);
-            cmd.ExecuteNonQuery();
-            cmd.Dispose();
-            conn.Close();
+            await ShoppingCart.Delete(id);
         }
     }
 }
