6419548e9d1693f20f558596dbffde5095648429
denizkanmaz <denizkanmaz@gmail.com>
2025-01-15 12:45:05 +0200

    feat: add endpoint for user manual pdf files

--- Controllers/Admin/ProductsController.cs
+++ Controllers/Admin/ProductsController.cs
@@ -10,19 +10,42 @@ namespace Turg.App.Controllers.Admin
         {
             var products = await Product.GetAll();
 
-            ViewData.Model = products;
+            // ViewData.Model = products;
 
-            var viewResult = new ViewResult()
+            // var viewResult = new ViewResult()
+            // {
+            //     ViewName = "/Views/Admin/Products/Index.cshtml",
+            //     ViewData = ViewData,
+            // };
+
+            // // await viewResult.ExecuteResultAsync(ControllerContext);
+
+            // return viewResult;
+
+            return View("/Views/Admin/Products/Index.cshtml", products);
+        }
+
+        [HttpGet("{productId}/manual")]
+        public ActionResult Manual([FromRoute] Guid productId)
+        {
+            var fileName = $"manual-{productId}.pdf";
+
+            // Sends a file that is not necessarily publicly accessible over http (e.g. wwwroot)
+            // PhysicalFileResult
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/manuals", fileName);
+
+            if (!System.IO.File.Exists(filePath))
             {
-                ViewName = "/Views/Admin/Products/Index.cshtml",
-                ViewData = ViewData,
-            };
+                // return new NotFoundResult();
+                // await NotifyMisssingDocument(productId)
+                return NotFound();
+            }
 
-            // await viewResult.ExecuteResultAsync(ControllerContext);
+            // var result = new VirtualFileResult($"/manuals/{fileName}", "application/pdf");
+            // return result;
 
-            return viewResult;
-
-            // return View("/Views/Admin/Products/Index.cshtml", products);
+            return File($"/manuals/{fileName}", "application/pdf");
         }
     }
 }
