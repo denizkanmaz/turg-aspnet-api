41cefca58e4ed5ae998c653803ed261fefd65c57
denizkanmaz <denizkanmaz@gmail.com>
2025-02-21 15:19:00 +0200

    refactor: use strongly typed results

--- Endpoints/ShoppingCartEndpoints.cs
+++ Endpoints/ShoppingCartEndpoints.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Turg.App.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Turg.App.Endpoints;
 
@@ -8,31 +9,7 @@ internal class ShoppingCartEndpoints : IEndpoints
     public void Map(IEndpointRouteBuilder routeBuilder)
     {
         var shoppingCartGroup = routeBuilder.MapGroup("/shoppingcarts");
-        shoppingCartGroup.MapGet("/{id}", async (HttpContext context, string id) =>
-        {
-            var shoppingCart = await ShoppingCart.GetById(id);
-
-            if (shoppingCart == null)
-            {
-                // MVC: IActionResult
-                // Minimal API: IResult
-
-                // return Results.NotFound();
-                return Results.Problem(
-                    title: "Not Found",
-                    statusCode: 404,
-                    extensions: new Dictionary<string, object>{
-                        {
-                            "traceId", Activity.Current?.Id ?? context.TraceIdentifier
-                            // Activity.Current.Id: W3C-complient | distributed-tracing (e.g. microservices) | Diagnostics
-                            // HttpContext.TraceIdentifier:  // For tracking an individual local request.
-                        },
-                    }
-                );
-            }
-
-            return Results.Ok(shoppingCart);
-        });
+        shoppingCartGroup.MapGet("/{id}", GetById);
 
         shoppingCartGroup.MapPost("/{id}/items", async (Guid id, ShoppingCartItem shoppingCartItem) =>
         {
@@ -44,5 +21,25 @@ internal class ShoppingCartEndpoints : IEndpoints
         {
             await ShoppingCart.Delete(id);
         });
+    }
+
+    public static async Task<Results<ProblemHttpResult, Ok<ShoppingCart>>> GetById(HttpContext context, string id)
+    {
+        var shoppingCart = await ShoppingCart.GetById(id);
+
+        if (shoppingCart == null)
+        {
+            return TypedResults.Problem(
+                title: "Not Found",
+                statusCode: 404,
+                extensions: new Dictionary<string, object>{
+                        {
+                            "traceId", Activity.Current?.Id ?? context.TraceIdentifier
+                        },
+                }
+            );
+        }
+
+        return TypedResults.Ok(shoppingCart);
     }
 }
