c406517e5b1af44a1f52adeadaf59b467cc17eb6
denizkanmaz <denizkanmaz@gmail.com>
2025-02-21 10:34:27 +0200

    bug: shoppingcart should return 404 instead of 200 when resource does not exist

--- Endpoints/ShoppingCartEndpoints.cs
+++ Endpoints/ShoppingCartEndpoints.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Turg.App.Models;
 
 namespace Turg.App.Endpoints;
@@ -7,17 +8,30 @@ internal class ShoppingCartEndpoints : IEndpoints
     public void Map(IEndpointRouteBuilder routeBuilder)
     {
         var shoppingCartGroup = routeBuilder.MapGroup("/shoppingcarts");
-        shoppingCartGroup.MapGet("/{id}", async (string id) =>
+        shoppingCartGroup.MapGet("/{id}", async (HttpContext context, string id) =>
         {
             var shoppingCart = await ShoppingCart.GetById(id);
 
             if (shoppingCart == null)
             {
-                // return NotFound();
-                return null;
+                // MVC: IActionResult
+                // Minimal API: IResult
+
+                // return Results.NotFound();
+                return Results.Problem(
+                    title: "Not Found",
+                    statusCode: 404,
+                    extensions: new Dictionary<string, object>{
+                        {
+                            "traceId", Activity.Current?.Id ?? context.TraceIdentifier
+                            // Activity.Current.Id: W3C-complient | distributed-tracing (e.g. microservices) | Diagnostics
+                            // HttpContext.TraceIdentifier:  // For tracking an individual local request.
+                        },
+                    }
+                );
             }
 
-            return shoppingCart;
+            return Results.Ok(shoppingCart);
         });
 
         shoppingCartGroup.MapPost("/{id}/items", async (Guid id, ShoppingCartItem shoppingCartItem) =>
